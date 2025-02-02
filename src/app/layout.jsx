132c54cb59aec2df9883ddaba86b00bb77b1f023
Bohdan Khomych <bod.khomych@gmail.com>
2025-02-02 17:44:38 +0200

    rename app

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -12,7 +12,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: 'test-task-develops-today',
+  title: 'Car Dealer App',
 };
 
 export default function RootLayout({ children }) {
